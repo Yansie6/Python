691b7195ef7cbd13bddf5aaa3fc9ce9f96abde75
Yansie6 <yan.siegers@tele2.nl>
2014-08-16 23:13:20 +0200

    Create Ja-nee-lijsten.py
    Lijsten voor mogelijke ja/nee-antwoorden in het Nederlands.
    Lists for possibly yes/no-answers in Dutch.

---
+++ Ja-nee-lijsten.py
@@ -0,0 +1,3 @@
+ja  = ["j","ja","jah","jep","y","yes","yep","oui","aye","wel","jawel",
+      "oke","okee","ok","k","goed","best","si","jawol","mja"]:
+nee = ["n","nee","no","nope","nop","nein","non","nay","meh","niet","net"]
